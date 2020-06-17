318f6a6440e6d1cd71ef7bdbc14fb9decfcea14e
Mirai Hattori <mirai2045@gmail.com>
2020-06-17 09:40:35 +0900

    add THIRD env

--- scripts/second_gazebo.sh
+++ scripts/second_gazebo.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./run_docker.sh roslaunch robot_assembler robot_assembler_gazebo.launch model:=/userdir/SECOND.urdf
+./run_docker.sh roslaunch robot_assembler robot_assembler_gazebo.launch model:=/userdir/SECOND.urdf world_name:=table_manipulation.world
