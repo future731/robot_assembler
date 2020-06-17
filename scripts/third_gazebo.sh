f55809c0e6bd2c1ed06e3dc51c729794c635914c
Mirai Hattori <mirai2045@gmail.com>
2020-06-17 09:40:35 +0900

    add THIRD env

---
+++ scripts/third_gazebo.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./run_docker.sh roslaunch robot_assembler robot_assembler_gazebo.launch model:=/userdir/THIRD.urdf world_name:=table_manipulation.world
---
+++ scripts/third_stereo.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ROS_NAMESPACE=stereo_camera rosrun stereo_image_proc stereo_image_proc
